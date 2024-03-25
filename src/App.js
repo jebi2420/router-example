66152a4a97206e4ff624a854c20f5fc28b0d2694
jebi2420 <jebi2420@gmail.com>
2024-03-25 15:20:18 +0900

    [add] Redirect : 페이지를 보호하는 법

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+// Navigate는 리다이렉트하게 도와주는 컴포넌트
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Homepage from './page/Homepage';
 import Aboutpage from './page/Aboutpage';
 import ProductPage from './page/ProductPage';
 import ProductDetailPage from './page/ProductDetailPage';
+import LoginPage from './page/LoginPage';
+import UserPage from './page/UserPage';
 
 function App() {
+  // authenticate값이 false면 로그인 안 한거, true면 한 걸로 가정
+  const [authenticate, setAuthenticate] = useState(false)
+  // 컴포넌트
+  const PrivateRoute = () => {
+    return authenticate === true?<UserPage />:<Navigate to="/login" />
+  }
   return (
     <div>
       <Routes>
@@ -13,6 +23,8 @@ function App() {
         <Route path="/about" element={<Aboutpage />} />
         <Route path="/products" element={<ProductPage />} />
         <Route path="/products/:id" element={<ProductDetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/user" element={<PrivateRoute/>} />
       </Routes>
       
       

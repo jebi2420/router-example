0bf2601a6006f67e23c7b6165f42e59a390db157
jebi2420 <jebi2420@gmail.com>
2024-03-25 14:17:25 +0900

    [add] Restful Route 적용

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './page/Homepage';
 import Aboutpage from './page/Aboutpage';
+import ProductPage from './page/ProductPage';
+import ProductDetail from './page/ProductDetail';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="about" element={<Aboutpage />} />
+        <Route path="/about" element={<Aboutpage />} />
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
       </Routes>
       
       

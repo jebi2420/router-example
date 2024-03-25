6c6f5da568fd93f5baec9b78cc60615385336b3f
jebi2420 <jebi2420@gmail.com>
2024-03-25 13:34:24 +0900

    [add] Router 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Homepage from './page/Homepage';
+import Aboutpage from './page/Aboutpage';
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="about" element={<Aboutpage />} />
+      </Routes>
+      
       
     </div>
   );
